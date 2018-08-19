df4308ad0299b2e9ead5ce898dad7bfcdc920652
Suppasek <59070168@it.kmitl.ac.th>
2018-08-19 15:33:55 +0700

    Initial commit

---
+++ app/src/main/java/com/example/suppasek/healthy/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.suppasek.healthy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.fragment_login);
+        super.onCreate(savedInstanceState);
+        TextView regisButton =  findViewById(R.id.register);
+        regisButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
